d78ee987176e0667367f9c4c2904fcb7065246cc
Irina Semakova <semakovais@yandex.ru>
2025-05-27 22:08:29 +0300

    Deploy to GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,46 @@
-const path = require('path');
+
+const path = require('node:path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Точка входа приложения
   output: {
-    filename: 'bundle.js', // Имя выходного файла
-    path: path.resolve(__dirname, 'dist'), // Путь к выходной директории
-    clean: true, // Очищаем dist перед каждой сборкой
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Обработка JavaScript файлов через Babel
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'], // Преобразование современного JS в совместимый
-          },
         },
       },
       {
-        test: /\.css$/, // Обработка CSS файлов
-        use: ['style-loader', 'css-loader'], // Подключение стилей в DOM
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Обработка изображений
-        type: 'asset/resource', // Используем Asset Modules (Webpack 5+)
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
     ],
   },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'), // Статические файлы для DevServer
-    compress: true, // Включение gzip-сжатия
-    port: 3000, // Порт для разработки
-    open: true, // Автоматическое открытие браузера
-  },
-  resolve: {
-    extensions: ['.js'], // Разрешение расширений
-  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    })
+  ],
 };
